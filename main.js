aa7a15b970288b9673b082e3a86d13cb45d86a33
Danil Konovalov <47856599+danil4905@users.noreply.github.com>
2020-01-22 14:45:04 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,74 @@
-const goods = [
-    { title: "Робот-пылесос xiaomi", price: 20000, img: 'https://via.placeholder.com/150' },
-    { title: "Samsung Galaxy", price: 21500, img: 'https://via.placeholder.com/150' },
-    { title: "Стиральная машина hotpoint", price: 32000, img: 'https://via.placeholder.com/150' },
-    { title: "Умные часы apple watch", price: 26000, img: 'https://via.placeholder.com/150' },
-];
+class GoodsItem {
+    constructor(id, title = 'Без названия', price = 0, img = '') {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.img = img;
+    }
+    render() {
+        return `
+            <div class="goods-item" data-id="${this.id}">
+                <img src="${this.img}" alt="alt">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="js-add-to-cart">Добавить</button>
+            </div>
+        `;
+    }
+}
 
-const renderGoodsItem = (title='Без названия', price = 0, img = '') => {
-    return `<div class="goods-item">
-                <img src="${img}" alt="alt">
-                <h3>${title}</h3>
-        <p>${price}</p>
-            </div>`
-};
+class GoodsList {
+    constructor(container) {
+        this.container = document.querySelector(container);
+        this.goods = [];
+    }
+    initListeners() {
+        const buttons = [...this.container.querySelectorAll('.js-add-to-cart')];
+        buttons.forEach(button => {
+            button.addEventListener('click', (event) => {
+                const goodId = event.target.parentElement.getAttribute('data-id');
+                this.addToCart(parseInt(goodId, 10));
+            })
+        })
+    }
+    findGood(id) {
+        return this.goods.find(good => good.id === id);
+    }
+    addToCart(goodId) {
+        const good = this.findGood(goodId);
+        console.log(good);
+    }
+    fetchGoods() {
+        this.goods = [
+            {id: 1, title: "Робот-пылесос xiaomi", price: 20000, img: 'https://via.placeholder.com/250'},
+            {id: 2, title: "Samsung Galaxy", price: 21500, img: 'https://via.placeholder.com/250'},
+            {id: 3, title: "Стиральная машина hotpoint", price: 32000, img: 'https://via.placeholder.com/250'},
+            {id: 4, title: "Умные часы apple watch", price: 26000, img: 'https://via.placeholder.com/250'},
+            {id: 5, title: "Посудомоечная машина bosh", price: 26000, img: 'https://via.placeholder.com/250'},
+        ]
+    }
+    sumOfGoods() {
+        let sum = 0;
+        this.goods.forEach(good => {
+            sum+=good.price;
+        });
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.id, good.title, good.price, good.img);
+            listHtml += goodItem.render();
+        });
+        this.container.innerHTML = listHtml;
+        this.initListeners();
+    }
+}
+class Cart {
+    
+}
+class CartItem {
 
-const renderGoodsList = (list, container) => {
-    const goodsList = list.map(good => renderGoodsItem(good.title, good.price, good.img));
-    document.querySelector(container).innerHTML = goodsList;
-};
-
-renderGoodsList(goods, '.goods-list');
+}
+const list = new GoodsList('.goods-list');
+list.fetchGoods();
+list.render();
